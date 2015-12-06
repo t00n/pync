d58e652396ba217bf6cac9b8aa4136873a7568c1
t00n <kelkununtel@hotmail.com>
2015-12-06 19:06:25 +0100

    [add] pattern matching on lists

--- pync.py
+++ pync.py
@@ -9,3 +9,22 @@ def curry(func):
         else:
             return newfunc
     return f
+
+class MatchObject:
+    def __init__(self, obj):
+        self.obj = obj
+
+    def __getitem__(self, key):
+        if isinstance(key, tuple):
+            if isinstance(self.obj, list):
+                lst = [self.obj[i] for i in key]
+                lst.append([x for i, x in enumerate(self.obj) if i not in key])
+                return tuple(lst)
+        return obj.__getitem__[key]
+
+def listmatching(func):
+    def wrapper(*args, **kwargs):
+        newargs = map(MatchObject, args)
+        newkwargs = {key: MatchObject(val) for key, val in kwargs.items()}
+        return func(*newargs, **newkwargs)
+    return wrapper
--- test.py
+++ test.py
@@ -14,3 +14,14 @@ def test_curry():
     with pytest.raises(TypeError):
         a()
         testfunc()
+
+def test_listmatching():
+    @listmatching
+    def head(lst):
+        x, xs = lst[0,]
+        return x
+    testlist = list(range(5))
+    assert head(testlist) == 0
+    assert MatchObject(testlist)[0,1] == (0,1,[2,3,4])
+    assert MatchObject(testlist)[1,3] == (1,3,[0,2,4])
+
